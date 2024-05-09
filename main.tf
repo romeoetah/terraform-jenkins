660029e8973a369dbb5e025c935bd0b9d4e5e8af
Romeo Etah <romeoetah5@gmail.com>
2024-05-09 09:59:39 -0400

    first commit

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "name" {
   ami             = data.aws_ami.linux2.id
   instance_type   = "t2.medium"
   security_groups = [aws_security_group.jenkins.name]
-  key_name        = "jenkins"
+  key_name        = "calikey000"
 
   tags = {
-    Name = "jenkins-zikora"
+    Name = "jenkins-prince"
   }
   user_data = file("jenkins-install.sh")
 }
--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 
 
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-west-1"
   profile = "default"
 }
 
